cd06ddc7eb9ea839488e2a74f23ac040a0d6e5da
irwandandka <irwndandka@gmail.com>
2024-11-09 13:02:54 +0700

    Add more seeder and adjust the factory count

--- database/seeders/CitySeeder.php
+++ database/seeders/CitySeeder.php
@@ -13,6 +13,6 @@ class CitySeeder extends Seeder
      */
     public function run(): void
     {
-        City::factory()->count(30)->create();
+        City::factory()->count(10)->create();
     }
 }
--- database/seeders/RegionSeeder.php
+++ database/seeders/RegionSeeder.php
@@ -13,6 +13,6 @@ class RegionSeeder extends Seeder
      */
     public function run(): void
     {
-        Region::factory()->count(5)->create();
+        Region::factory()->count(3)->create();
     }
 }
