6242a5d7e066865babbf4ac584e6de3c3124f774
irwandandka <irwndandka@gmail.com>
2024-11-09 13:03:59 +0700

    Add region routing and refactor routing for GET endpoint

--- routes/api.php
+++ routes/api.php
@@ -23,14 +23,19 @@ Route::prefix('v1')->group(function () {
     });
 
     Route::prefix('/country')->group(function () {
-        Route::get('/', [CountryController::class, 'list']);
+        Route::get('/list', [CountryController::class, 'list']);
         Route::get('/{country}', [CountryController::class, 'show']);
+    });
+
+    Route::prefix('/region')->group(function () {
+        Route::get('/list', [RegionController::class, 'list']);
+        Route::get('/{region}', [RegionController::class, 'show']);
     });
 
     Route::prefix('/city')->group(function () {
         Route::get('/deleted', [CityController::class, 'getDeleted']);
         Route::get('/{city}', [CityController::class, 'show']);
-        Route::get('/', [CityController::class, 'list']);
+        Route::get('/list', [CityController::class, 'list']);
         Route::delete('/{city}', [CityController::class, 'delete']);
     });
 
