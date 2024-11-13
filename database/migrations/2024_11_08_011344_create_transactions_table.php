f64954171390b2734b97be9a7872c4bc3fcbe4a4
irwandandka <irwndandka@gmail.com>
2024-11-13 23:09:46 +0700

    Migrations

---
+++ database/migrations/2024_11_08_011344_create_transactions_table.php
@@ -0,0 +1,56 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('user_id');
+            $table->uuid('product_id');
+            $table->uuid('status_id');
+            $table->string('code');
+            $table->string('quantity');
+            $table->double('total_price');
+            $table->date('booking_date');
+            $table->string('notes');
+            $table->timestamps();
+
+            $table
+                ->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+
+            $table
+                ->foreign('product_id')
+                ->references('id')
+                ->on('products')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+
+            $table
+                ->foreign('status_id')
+                ->references('id')
+                ->on('statuses')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
---
+++ database/migrations/2024_11_08_011401_create_payments_table.php
@@ -0,0 +1,72 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('transaction_id');
+            $table->uuid('currency_id');
+            $table->uuid('payment_method_id');
+            $table->uuid('user_id');
+            $table->uuid('status_id');
+            $table->double('amount');
+            $table->dateTime('payment_date');
+            $table->dateTime('due_date');
+            $table->string('payment_reference');
+            $table->text('notes');
+            $table->timestamps();
+
+            $table
+                ->foreign('transaction_id')
+                ->references('id')
+                ->on('transactions')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+
+            $table
+                ->foreign('currency_id')
+                ->references('id')
+                ->on('currencies')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+
+            $table
+                ->foreign('payment_method_id')
+                ->references('id')
+                ->on('payment_methods')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+
+            $table
+                ->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+
+            $table
+                ->foreign('status_id')
+                ->references('id')
+                ->on('statuses')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
