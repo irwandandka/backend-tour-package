6b73eab1b22b0261244bf68afec30acc9434a3b9
irwandandka <irwndandka@gmail.com>
2024-11-25 21:13:13 +0700

    Tour agent migration

---
+++ database/migrations/2024_11_13_153115_create_tour_agents_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tour_agents', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('name');
+            $table->string('email');
+            $table->string('phone_number');
+            $table->text('address');
+            $table->uuid('status_id');
+            $table->timestamps();
+
+            $table
+                ->foreign('status_id')
+                ->references('id')
+                ->on('statuses')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tour_agents');
+    }
+};
