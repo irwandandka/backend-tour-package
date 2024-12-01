a9b37dfcfac64194fe4e69c96857f2fee699fccc
irwandandka <irwndandka@gmail.com>
2024-12-02 06:16:23 +0700

    Log

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    Log::channel('testing')->info('Route Accessed');
     return view('welcome');
 });
