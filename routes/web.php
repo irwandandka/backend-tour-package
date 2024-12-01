31f1ba0f48ab08b78f347d3dc77ed75543d8e79b
irwandandka <irwndandka@gmail.com>
2024-12-01 11:45:52 +0700

    Plis Capek

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/laravel-cibaii', function () {
+    return 'Laravel CIBAI';
+});
