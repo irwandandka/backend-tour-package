774448a1a1b30710a92524e705e43a87848fe34c
irwandandka <irwndandka@gmail.com>
2024-12-25 23:15:17 +0700

    Endpoint for Explore Now

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Classes\ApiResponseClass;
 use App\Http\Resources\ProductResource;
 use App\Models\Product;
+use App\Models\City;
 use Exception;
 use Illuminate\Http\Request;
 use App\Services\ErrorHandler;
@@ -55,6 +56,46 @@ class ProductController extends Controller
             return response()->json([
                 'status' => 'success',
                 'data' => $popularDestinations,
+            ]);
+        } catch (Exception $e) {
+            $this->errorHandler->handleError($e);
+        }
+    }
+
+    public function exploreNow(Request $request)
+    {
+        try {
+            $cities = City::with(['products.reviews']) // Load products and reviews
+                ->get()
+                ->map(function ($city) {
+                    // Calculate the average rating for each city based on its products' reviews
+                    $reviews = $city->products->flatMap(function ($product) {
+                        return $product->reviews;
+                    });
+
+                    // Check if reviews exist for the city
+                    $averageRating = $reviews->isEmpty() ? 0 : round($reviews->avg('rating'), 1);
+
+                    // Add the average rating to the city
+                    $city->average_rating = $averageRating;
+
+                    return [
+                        'id' => $city->id,
+                        'name' => $city->name,
+                        'rating' => $averageRating, // Return the calculated average rating
+                    ];
+                })
+                ->filter(function ($city) {
+                    // Exclude cities with 0 rating
+                    return $city['rating'] > 0;
+                })
+                ->sortByDesc('rating') // Sort by rating in descending order
+                ->values() // Re-index the array after filtering
+                ->take(5);
+
+            return response()->json([
+                'status' => 'success',
+                'data' => $cities,
             ]);
         } catch (Exception $e) {
             $this->errorHandler->handleError($e);
--- routes/api.php
+++ routes/api.php
@@ -38,6 +38,7 @@ Route::prefix('v1')->group(function () {
     Route::prefix('/product')->group(function () {
         Route::get('/list', [ProductController::class, 'list']);
         Route::get('/popular-destination', [ProductController::class, 'popularDestination']);
+        Route::get('/explore-now', [ProductController::class, 'exploreNow']);
         Route::get('/{slug}', [ProductController::class, 'show']);
     });
 
