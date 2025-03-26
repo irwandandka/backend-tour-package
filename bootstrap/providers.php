072546231c2dea3258dc10867152a42891c835c1
irwandandka <irwndandka@gmail.com>
2025-03-26 23:46:12 +0700

    Setup Filament

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,5 +2,7 @@
 
 return [
     App\Providers\AppServiceProvider::class,
-        App\Providers\PrometheusServiceProvider::class,
+    App\Providers\Filament\AcetoursPanelProvider::class,
+    App\Providers\Filament\IrwandandkaPanelProvider::class,
+    App\Providers\PrometheusServiceProvider::class,
 ];
