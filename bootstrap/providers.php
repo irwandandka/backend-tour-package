3981facf9c9f6e4cf1e5a1db985d8ebef326c867
irwandandka <irwndandka@gmail.com>
2025-05-02 20:55:07 +0700

    fix: Filament - remove Irwandandka panel

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,6 +3,5 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\AcetoursPanelProvider::class,
-    App\Providers\Filament\IrwandandkaPanelProvider::class,
     App\Providers\PrometheusServiceProvider::class,
 ];
