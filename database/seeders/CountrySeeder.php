02397914479871f83187ada4e534c06f5ac214ef
irwandandka <irwndandka@gmail.com>
2025-06-16 23:53:09 +0700

    fix: Seeder - Update Country & City Seeder

--- database/seeders/CountrySeeder.php
+++ database/seeders/CountrySeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Country;
+use App\Models\Region;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,90 +14,91 @@ class CountrySeeder extends Seeder
      */
     public function run(): void
     {
+        $regions = Region::all();
         $countries = [
             [
                 'name' => 'South Korea',
                 'iso_code' => 'KR',
                 'phone_code' => '+82',
-                'region_id' => '55e2b614-3182-40c3-8ae3-b72582822747',
+                'region_id' => $regions->where('code', 'as')->first()->id,
             ],
             [
                 'name' => 'Japan',
                 'iso_code' => 'JP',
                 'phone_code' => '+81',
-                'region_id' => '55e2b614-3182-40c3-8ae3-b72582822747',
+                'region_id' => $regions->where('code', 'as')->first()->id,
             ],
             [
                 'name' => 'China',
                 'iso_code' => 'CN',
                 'phone_code' => '+86',
-                'region_id' => '55e2b614-3182-40c3-8ae3-b72582822747',
+                'region_id' => $regions->where('code', 'as')->first()->id,
             ],
             [
                 'name' => 'Indonesia',
                 'iso_code' => 'ID',
                 'phone_code' => '+62',
-                'region_id' => '968659f9-98d3-4df9-973a-6c84b92a88a6',
+                'region_id' => $regions->where('code', 'sea')->first()->id,
             ],
             [
                 'name' => 'Thailand',
                 'iso_code' => 'TH',
                 'phone_code' => '+66',
-                'region_id' => '968659f9-98d3-4df9-973a-6c84b92a88a6',
+                'region_id' => $regions->where('code', 'sea')->first()->id,
             ],
             [
                 'name' => 'Vietnam',
                 'iso_code' => 'VN',
                 'phone_code' => '+84',
-                'region_id' => '968659f9-98d3-4df9-973a-6c84b92a88a6',
+                'region_id' => $regions->where('code', 'sea')->first()->id,
             ],
             [
                 'name' => 'Malaysia',
                 'iso_code' => 'MY',
                 'phone_code' => '+60',
-                'region_id' => '968659f9-98d3-4df9-973a-6c84b92a88a6',
+                'region_id' => $regions->where('code', 'sea')->first()->id,
             ],
             [
                 'name' => 'Singapore',
                 'iso_code' => 'SG',
                 'phone_code' => '+65',
-                'region_id' => '968659f9-98d3-4df9-973a-6c84b92a88a6',
+                'region_id' => $regions->where('code', 'sea')->first()->id,
             ],
             [
                 'name' => 'Italy',
                 'iso_code' => 'IT',
                 'phone_code' => '+39',
-                'region_id' => '31fbf1d6-2017-44ff-a2b9-3d2480499d42',
+                'region_id' => $regions->where('code', 'eu')->first()->id,
             ],
             [
                 'name' => 'United Kingdom',
                 'iso_code' => 'GB',
                 'phone_code' => '+44',
-                'region_id' => '31fbf1d6-2017-44ff-a2b9-3d2480499d42',
+                'region_id' => $regions->where('code', 'eu')->first()->id,
             ],
             [
                 'name' => 'Portugal',
                 'iso_code' => 'PT',
                 'phone_code' => '+351',
-                'region_id' => '31fbf1d6-2017-44ff-a2b9-3d2480499d42',
+                'region_id' => $regions->where('code', 'eu')->first()->id,
             ],
             [
                 'name' => 'Saudi Arabia',
                 'iso_code' => 'SA',
                 'phone_code' => '+966',
-                'region_id' => 'e07688c0-9abf-4185-b682-85314cde48fd',
+                'region_id' => $regions->where('code', 'me')->first()->id,
             ],
             [
                 'name' => 'Egypt',
                 'iso_code' => 'EG',
                 'phone_code' => '+20',
-                'region_id' => 'e07688c0-9abf-4185-b682-85314cde48fd',
+                'region_id' => $regions->where('code', 'me')->first()->id,
             ],
             [
                 'name' => 'Kuwait',
                 'iso_code' => 'KW',
                 'phone_code' => '+965',
-                'region_id' => 'e07688c0-9abf-4185-b682-85314cde48fd',
+                'region_id' => $regions->where('code', 'me')->first()->id,
             ]
         ];
         foreach ($countries as $country) {
