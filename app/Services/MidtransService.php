931b3fc30016fc9e107985acc2bae01805c3fa3f
irwandandka <irwndandka@gmail.com>
2025-08-24 15:58:32 +0700

    feat: Midtrans Service

---
+++ app/Services/MidtransService.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Payment;
+use App\Models\Transaction;
+use Midtrans\Config;
+use Midtrans\Snap;
+use Throwable;
+use Illuminate\Http\Request;
+
+class MidtransService
+{
+    private $errorHandler;
+
+    public function __construct()
+    {
+        Config::$serverKey = config('midtrans.server_key');
+        Config::$isProduction = config('midtrans.is_production');
+        Config::$isSanitized = config('midtrans.is_sanitize');
+        Config::$is3ds = config('midtrans.is_3ds');
+
+        $this->errorHandler = new ErrorHandler;
+    }
+
+    public function createTransaction(array $params)
+    {
+        try {
+            return Snap::createTransaction($params);
+        } catch (Throwable $e) {
+            return $this->errorHandler->handle($e);
+        }
+    }
+}
