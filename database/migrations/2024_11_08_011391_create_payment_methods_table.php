0e7bd8f3b124f6a7b1881fa13dbf34feaefd5af8
irwandandka <irwndandka@gmail.com>
2025-08-24 19:21:20 +0700

    feat: Payment Method - add code field

--- database/migrations/2024_11_08_011391_create_payment_methods_table.php
+++ database/migrations/2024_11_08_011391_create_payment_methods_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('payment_methods', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->string('name');
+            $table->string('code')->unique();
             $table->text('description');
             $table->boolean('is_active');
             $table->timestamps();
